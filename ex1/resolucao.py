10ef65b43f62c0bf9ecdb24681c355107fd4a37c
Leonardo <leomail2479@gmail.com>
2019-12-17 19:49:52 -0300

    ex1 pronto

---
+++ ex1/resolucao.py
@@ -0,0 +1,4 @@
+palavra = input('digite uma palavra! ' )
+palavra += ' '
+print(f'{palavra * 9 }\n{palavra * 9 }\n{palavra * 9 }\n{palavra * 9 }'
+      f'\n{palavra * 9 }\n{palavra * 9 }\n{palavra * 9 }\n{palavra * 9 }\n{palavra * 9 }')
---
+++ ex2/resolucao.py
@@ -0,0 +1,8 @@
+def substitui(cadeia):
+    vogais = "aeiou"
+    for car in vogais:
+        cadeia = cadeia.replace(car, "@")
+    print(cadeia)
+    return cadeia
+
+print(substitui('cadeia'))
